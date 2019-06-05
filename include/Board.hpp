f9452a7baf15ea4c55de64bb4ab963bf11ef8546
zer0nim <emarin@student.42.fr>
2019-06-05 11:21:54 +0200

    GET_ST/PUT_ST, GET_VUL/SET_VUL define for Board access

--- include/Board.hpp
+++ include/Board.hpp
@@ -4,12 +4,19 @@
 # include "Define.hpp"
 # include <array>
 
+# define GET_ST(boardC, x, y) ((boardC[y] & (3ull << (x*3))) >> (x*3))
+# define SET_ST(boardC, x, y, val) (boardC[y] = (boardC[y] & (((1ull << (BOARD_SZ*3))-1) ^ (3ull << (x*3)))) | (static_cast<uint64_t>(val) << (x*3)))
+
+# define GET_VUL(boardC, x, y) ((boardC[y] & (4ull << (x*3))) >> (x*3 + 2))
+# define SET_VUL(boardC, x, y, val) (boardC[y] = (boardC[y] & (((1ull << (BOARD_SZ*3))-1) ^ (4ull << (x*3)))) | (static_cast<uint64_t>(val) << (x*3 + 2)))
+
+
 class Board {
 	public:
 		Board();
 		virtual	~Board();
 	private:
-		std::array<char, BOARD_SZ> _content;
+		std::array<uint64_t, BOARD_SZ> _content;
 };
 
 #endif
--- src/Board.cpp
+++ src/Board.cpp
@@ -3,8 +3,49 @@
 
 Board::Board() {
 	std::cout << "content size: " << _content.size() << std::endl;
-	_content[1] = 42;
-	std::cout << "_content[1]: " << _content[1] << std::endl;
+	std::cout << "__SET_ST( 0 ) to 0:0=> stone: ";
+	SET_ST(_content, 1, 1, 0);
+	std::cout << GET_ST(_content, 1, 1) << std::endl;
+
+	std::cout << "__SET_ST( 1 ) to 0:0=> stone: ";
+	SET_ST(_content, 1, 1, 1);
+	std::cout << GET_ST(_content, 1, 1) << std::endl;
+
+	std::cout << "__SET_ST( 2 ) to 0:0=> stone: ";
+	SET_ST(_content, 1, 1, 2);
+	std::cout << GET_ST(_content, 1, 1) << std::endl;
+	std::cout << std::endl;
+
+
+	std::cout << "__SET_ST( 0 ) to 18:5=> stone: ";
+	SET_ST(_content, 18, 5, 0);
+	std::cout << GET_ST(_content, 18, 5) << std::endl;
+
+	std::cout << "__SET_ST( 1 ) to 18:5=> stone: ";
+	SET_ST(_content, 18, 5, 1);
+	std::cout << GET_ST(_content, 18, 5) << std::endl;
+
+	std::cout << "__SET_ST( 2 ) to 18:5=> stone: ";
+	SET_ST(_content, 18, 5, 2);
+	std::cout << GET_ST(_content, 18, 5) << std::endl;
+	std::cout << std::endl;
+
+
+	std::cout << "test vulnerability__________________________" << std::endl;
+	std::cout << std::endl;
+
+
+	std::cout << "__SET_VUL( 0 ) to 18:5=> vuln: ";
+	SET_VUL(_content, 18, 5, 0);
+	std::cout << GET_VUL(_content, 18, 5) << ", stone: " << GET_ST(_content, 18, 5) << std::endl;
+
+	std::cout << "__SET_VUL( 1 ) to 18:5=> vuln: ";
+	SET_VUL(_content, 18, 5, 1);
+	std::cout << GET_VUL(_content, 18, 5) << ", stone: " << GET_ST(_content, 18, 5) << std::endl;
+
+	std::cout << "__SET_VUL( 0 ) to 18:5=> vuln: ";
+	SET_VUL(_content, 18, 5, 0);
+	std::cout << GET_VUL(_content, 18, 5) << ", stone: " << GET_ST(_content, 18, 5) << std::endl;
 }
 
 Board::~Board() {
